a3ee9f05c948669a5e295b014dce81fc3c9efab1
Monjurul Alam <monjurulalam190629@gmail.com>
2025-07-25 23:50:32 +0600

    solve group anagram

--- LeeCode_49_Group_Anagrams.py
+++ LeeCode_49_Group_Anagrams.py
@@ -1,1 +1,20 @@
-print("Hello world")
+def find_group_anagrams(strs):
+    visited = [False] * len(strs)
+    ans = []
+
+    for i in range(len(strs)):
+        if visited[i]:
+            continue
+        subAns = [strs[i]]
+        visited[i] = True
+        for j in range(i + 1, len(strs)):
+            if not visited[j] and sorted(strs[i]) == sorted(strs[j]):
+                subAns.append(strs[j])
+                visited[j] = True
+        ans.append(subAns)
+
+    return ans
+
+print(find_group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+
