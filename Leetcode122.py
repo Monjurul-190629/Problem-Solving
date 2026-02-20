713314152a77705a4b2880b58c44e072d5593243
monjurul-190629 <monjurulalam190629@gmail.com>
2026-02-21 03:16:15 +0600

    solved: leetcode 121

---
+++ Leetcode122.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        chosen_price = prices[0]
+        profit = 0
+        for i in prices[1:]:
+            if i < chosen_price:
+               chosen_price = i
+            else:
+               profit = max(profit, i - chosen_price)
+        return profit
+
+sol = Solution()
+print(sol.maxProfit([7,1,5,3,6,4]))
