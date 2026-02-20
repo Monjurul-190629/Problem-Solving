d903f5384e07f876849f4ed140db9986e1cf912f
monjurul-190629 <monjurulalam190629@gmail.com>
2026-02-20 20:06:28 +0600

    solved: merged sorted array

---
+++ 88_Merged_Sorted_Array.py
@@ -0,0 +1,30 @@
+# this is not optimal but accepted
+# class Solution:
+#     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+#         i = m
+#         j = n - 1
+#         k = 0
+#         while j >= 0 :
+#            nums1[i+k] = nums2[j]
+#            j -= 1
+#            k += 1
+#         nums1.sort()
+#         print(nums1)
+
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+# time complexity O(m + n)
+sol = Solution()
+sol.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
