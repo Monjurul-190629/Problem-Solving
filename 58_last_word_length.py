d7ad201ae7bebc439544fe3c2c4c3c31c8c7ca8e
monjurul-190629 <monjurulalam190629@gmail.com>
2026-04-11 23:39:48 +0600

    add: leetcode 35

--- 58_last_word_length.py
+++ 58_last_word_length.py
@@ -1,8 +1,24 @@
+# class Solution:
+#     def lengthOfLastWord(self, s: str) -> int:
+#         arr = s.strip().split()
+#         return len(arr[len(arr) - 1])
+        
+        
+# sol = Solution()
+# print(sol.lengthOfLastWord("   fly me   to   the moon  "))   time complexity: O(n) space complexity: O(n)
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        arr = s.strip().split()
-        return len(arr[len(arr) - 1])
+        length = 0
+        i = len(s) - 1
         
+        # Skip trailing spaces
+        while i >= 0 and s[i] == " ":
+            i -= 1
         
-sol = Solution()
-print(sol.lengthOfLastWord("   fly me   to   the moon  "))
+        # Count last word
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+        
+        return length   # time comp O(n) space complexity O(1)
