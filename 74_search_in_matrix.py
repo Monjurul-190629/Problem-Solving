7d1eb9facaf5bae586879a0c78f562ac9ee1612e
monjurul-190629 <monjurulalam190629@gmail.com>
2026-04-14 22:02:06 +0600

    solve: optimal solution of search in 2D matrix

--- 74_search_in_matrix.py
+++ 74_search_in_matrix.py
@@ -1,9 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
-        matrixlen = len(matrix)
-        for i in range(matrixlen):
-            if target in matrix[i]:
+        # brute force
+        # matrixlen = len(matrix)
+        # for i in range(matrixlen):
+        #     if target in matrix[i]:
+        #         return True
+        # return False
+        # optimal solution
+        if not matrix or not matrix[0]:
+            return False
+        m = len(matrix)
+        n = len(matrix[0])
+        start = 0
+        end = m * n - 1
+        while start <= end:
+            mid = (start + end) // 2
+            row = mid // n
+            col = mid % n
+            value = matrix[row][col]
+            if value == target:
                 return True
+            elif value < target:
+                start = mid + 1
+            elif value > target:
+               end = mid - 1
         return False
 
 
