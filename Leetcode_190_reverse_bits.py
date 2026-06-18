0c161829120f1ca9748487cb221404517657af68
monjurul-190629 <monjurulalam190629@gmail.com>
2026-06-18 17:03:25 +0600

    solve: 190 reverse bits with optimal solution

---
+++ Leetcode_190_reverse_bits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        # binaryForm = bin(n)[2:]
+        # binaryFormWithBits = binaryForm.zfill(32)
+        # reverseForm = binaryFormWithBits[::-1]
+        # ans = int(reverseForm, 2)
+        # return ans
+        # time complexity o(k) and space complexity o(1)
+        result = 0
+        for _ in range(32):
+            bit = n & 1
+            result = (result << 1) | bit
+            n = n >> 1
+        return result
+
+sol = Solution()
+print(sol.reverseBits(6))
